0038ff35e9e386da6237fd3b7c42482697fb0bc5
JuanjeNaMu <jnavmun917@g.educaand.es>
2025-12-28 19:18:11 +0100

    Proyecto Iniciado

--- App.tsx
+++ App.tsx
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, ScrollView, StyleSheet, View, Text, ImageBackground, useColorScheme, ImageSourcePropType, TextInput, Pressable, Modal, Alert, FlatList, TextInputComponent, ColorValue} from 'react-native'
+import { MaterialIcons } from '@expo/vector-icons'
+import { useFonts } from 'expo-font'
+import { Image } from 'expo-image'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import React, { ReactNode, useEffect, useState } from 'react'
+import axios from "axios";
+import dayjs from "dayjs";
+import { Picker } from '@react-native-picker/picker'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import * as R from 'ramda';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>App</Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({})
